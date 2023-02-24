0c777ab1bfbd7ce0c04f4607b49365cde5cf31c2
Milton Rubio <a3519110011@alumno.uttehuacan.edu.mx>
2023-02-24 03:37:05 -0600

    Cambios en los usuarios (fotos)

--- php/cruds/usuarios/actualizar.php
+++ php/cruds/usuarios/actualizar.php
@@ -11,6 +11,8 @@ $celular=$_POST['celular1'];
 $Clave=$_POST['clave1'];
 $correo=$_POST['correo1'];
 
+$foto=$_POST['imagen1'];
+
 
 
 $nombreConvert = strtr($Nombre, " ", "_");
@@ -22,11 +24,32 @@ $res1 = substr("$Apellidos",0, 2);
 $usuario = "$nombreConvert"."$res1"."$res";
 
 
-$sql="UPDATE usuarios SET nombre='$Nombre', apellidos='$Apellidos',cargo='$Cargo', celular='$celular',correo='$correo', clave='$Clave', usuario='$usuario' WHERE ID_usuario='$ID_usuario'";
-echo mysqli_query($con,$sql);
+
+$imagen = $_FILES['imagen1']['name'];
+$tipo = $_FILES['imagen1']['type'];
+$temp  = $_FILES['imagen1']['tmp_name'];
+
+
+
+
+if (!empty($imagen)){
+    $sql="UPDATE usuarios SET nombre='$Nombre', apellidos='$Apellidos',cargo='$Cargo', celular='$celular',correo='$correo', clave='$Clave', usuario='$usuario', foto='$imagen' WHERE ID_usuario='$ID_usuario'";
+
+    move_uploaded_file($temp,"C:/xampp/htdocs/Alba/img/usuarios/$imagen");
+   }
+   else if(empty($imagen)){
+    
+    $sql="UPDATE usuarios SET nombre='$Nombre', apellidos='$Apellidos',cargo='$Cargo', celular='$celular',correo='$correo', clave='$Clave', usuario='$usuario' WHERE ID_usuario='$ID_usuario'";
+
+   }
+
  
- 
- 
+$query=mysqli_query($con,$sql);
+
+echo "<script>  
+history.back();
+alert('Se enviaron los datos');
+</script>";
  ?>
 
 
